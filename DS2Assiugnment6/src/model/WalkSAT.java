3f42ea30835fce26d6d80d5a77d9418cc077dc07
David Spurlock <dspurlo1@my.westga.edu>
2018-12-11 20:09:42 -0500

    Updated the probabilities for flipping based off of the amount of clauses correct.

--- DS2Assiugnment6/src/model/WalkSAT.java
+++ DS2Assiugnment6/src/model/WalkSAT.java
@@ -5,52 +5,65 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class WalkSAT {
-	
-	
-	public static final String FILENAME = "SAT_Expression01.txt";
-	
+
+	public static final String FILENAME = "SAT_Expression03.txt";
+
 	private ArrayList<ArrayList<Integer>> clauses;
 	private ArrayList<Integer> variables;
 	private HashMap<Integer, Boolean> map;
 	private Random random;
 	private HashMap<ArrayList<Integer>, Boolean> satisfiedClauseMap;
-	
-	
+	private ArrayList<Integer> finalVariables;
+	private int flipsNotTaken;
+
 	public WalkSAT() {
 		this.random = new Random();
 		this.satisfiedClauseMap = new HashMap<ArrayList<Integer>, Boolean>();
+		this.finalVariables = new ArrayList<Integer>();
+		this.flipsNotTaken = 0;
 	}
-	
+
 	public boolean run() {
-		
+
 		this.populateClauses(FILENAME);
 		this.populateVariables();
 		this.initializeMap();
-		
+
 		boolean end = false;
 		int count = 1;
-		
-		while(!this.doesMapSatisfyClauses()) {
+
+		while (!this.doesMapSatisfyClauses()) {
 //			System.out.println("Satisfied: " + this.satisfiedClauseMap);
 //			System.out.println("Map: " + this.map);
 			int key;
 			int index = this.random.nextInt(this.variables.size());
 			key = this.variables.get(index);
+			
+			while(finalVariables.contains(key)) {
+				index = this.random.nextInt(this.variables.size());
+				key = this.variables.get(index);
+			}
+			
+			
 			this.tryFlip(key);
 			if (count % 1000000 == 0) {
-				System.out.println(String.format("%,d", count));
+				
 			}
-			if (count % 10000 == 0) {
+			if (count % 100000 == 0) {
+				System.out.println(String.format("%,d", count));
+				System.out.println("flips not taken = " + flipsNotTaken);
 				this.initializeMap();
+				flipsNotTaken = 0;
 			}
 
 			count++;
-		}		
-		
+		}
+
 		System.out.println();
 		System.out.println("Clauses: " + this.clauses);
 		System.out.println("Variables: " + this.variables);
@@ -58,49 +71,59 @@ public class WalkSAT {
 		System.out.println("Count: " + (String.format("%,d", count)));
 		System.out.println(this.doesMapSatisfyClauses());
 		return end;
-		
+
 	}
-	
-	
-	
+
 	private void tryFlip(Integer key) {
 
 		int lastCount = this.getSatisfiedCount();
+		//System.out.println(lastCount);
 		this.flipValueAt(key);
-		
+		doesMapSatisfyClauses();
+		//System.out.println(this.getSatisfiedCount());
 		if (lastCount > this.getSatisfiedCount()) {
-			int successAmount = this.satisfiedClauseMap.size() - lastCount;
-			if (successAmount > this.random.nextInt(this.map.keySet().size() + 1)) {
+			//System.out.println("Wrong Move");
+			//System.out.println("Last Count = " + lastCount);
+			//System.out.println("Current Count = " + this.getSatisfiedCount());
+			
+			float successChance = (float)1 - ((float)this.getSatisfiedCount() / (float)lastCount);
+			float successRoll = (float)(this.random.nextInt(100)) / (float)100;
+			//System.out.println(successRoll);
+			//System.out.println(successChance);
+			if(successRoll > successChance) {
 				this.flipValueAt(key);
+				flipsNotTaken++;
 			}
+			
+			//int successAmount = this.satisfiedClauseMap.size() - lastCount;
+			//if (successAmount > this.random.nextInt(this.map.keySet().size() + 1)) {
+			//	this.flipValueAt(key);
+			//}
 		}
 
 	}
-	
-	
+
 	private int getSatisfiedCount() {
 		Collection<Boolean> lastList = (Collection<Boolean>) this.satisfiedClauseMap.values();
 		return (int) lastList.stream().filter(t -> t == true).count();
 	}
-	
-	
+
 	private void flipValueAt(Integer key) {
 		Boolean currentValue = this.map.get(key);
 		this.map.put(key, !currentValue);
 	}
-	
-	
+
 	private boolean doesMapSatisfyClauses() {
 		boolean pass = true;
-		for(ArrayList<Integer> clause : this.clauses) {
+		for (ArrayList<Integer> clause : this.clauses) {
 			Boolean current = this.clauseValue(clause);
 			this.satisfiedClauseMap.put(clause, current);
 			pass = pass && current;
 		}
 		return pass;
 	}
-	
-	private boolean clauseValue(ArrayList<Integer> clause) {		
+
+	private boolean clauseValue(ArrayList<Integer> clause) {
 		for (Integer number : clause) {
 			boolean mapBool = this.map.get(Math.abs(number));
 			if (number < 0) {
@@ -110,11 +133,10 @@ public class WalkSAT {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
 	private void initializeMap() {
 		this.map = new HashMap<Integer, Boolean>();
 		Random random = new Random();
@@ -122,8 +144,26 @@ public class WalkSAT {
 			boolean randomValue = random.nextBoolean();
 			this.map.put(variable, randomValue);
 		}
+		setFinalVariables();
+		
 	}
 	
+	private void setFinalVariables() {
+		for(ArrayList<Integer> eachClause : this.clauses) {
+			if(eachClause.size() == 1) {
+				Integer thisVariable = eachClause.get(0);
+				this.finalVariables.add(Math.abs(thisVariable));
+				if(thisVariable < 0) {
+					this.map.put(Math.abs(thisVariable), false);
+				} else {
+					this.map.put(Math.abs(thisVariable), true);
+				}
+			}
+		}
+	}
+	
+	
+
 	private void populateVariables() {
 		this.variables = new ArrayList<Integer>();
 		for (ArrayList<Integer> clause : clauses) {
@@ -135,8 +175,7 @@ public class WalkSAT {
 			}
 		}
 	}
-	
-	
+
 	private void populateClauses(String filename) {
 		this.clauses = new ArrayList<ArrayList<Integer>>();
 
@@ -146,7 +185,7 @@ public class WalkSAT {
 			while (scanner.hasNextLine()) {
 				ArrayList<Integer> clause = new ArrayList<Integer>();
 				String[] splitLine = scanner.nextLine().split(" ");
-				
+
 				for (String integer : splitLine) {
 					clause.add(Integer.parseInt(integer));
 				}
@@ -156,6 +195,5 @@ public class WalkSAT {
 			e.printStackTrace();
 		}
 	}
-	
 
 }
